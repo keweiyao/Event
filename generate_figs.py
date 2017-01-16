7ff79e01fc2232c0049c2a657514970abe2e7337
keweiyao <wk42@phy.duke.edu>
2017-01-16 11:21:04 -0500

    output particle data

--- generate_figs.py
+++ generate_figs.py
@@ -21,19 +21,17 @@ def corner(ds, ranges, bins=50, title=''):
 			plt.subplot(N, N, i*N+j+1)
 			if i==j:
 				plt.hist(ds[i], bins=bins, range=[ranges[i,0], ranges[i,1]], histtype='step', normed=True)
-				H,x= np.histogram(ds[i], bins=bins, range=[ranges[i,0], ranges[i,1]])
-				#print np.sum(H)
 				if i==0:
 					
 					e = np.linspace(1.3, 1.3*10, 100)
 					de = e[1] - e[0]
-					dfde = dfdE(e, 0.4, 1.3)
+					dfde = dfdE(e, 0.3, 1.3)
 					dfde = dfde/np.sum(dfde)/de
 					plt.plot(e, dfde, 'r-', linewidth=2.0)
 				else:
 					p = np.linspace(-1.3*5, 1.3*5, 100)
 					dp = p[1] - p[0]
-					dfdp = dfdP(p, 0.4, 1.3)
+					dfdp = dfdP(p, 0.3, 1.3)
 					dfdp = dfdp/np.sum(dfdp)/dp
 					plt.plot(p, dfdp, 'r-', linewidth=2.0)
 				plt.xlim(ranges[i,0], ranges[i,1])
@@ -42,11 +40,39 @@ def corner(ds, ranges, bins=50, title=''):
 				plt.xlim(ranges[j,0], ranges[j,1])
 				plt.ylim(ranges[i,0], ranges[i,1])
 	plt.title(title)
-	plt.pause(0.05)
+	plt.pause(0.1)
 
-f = h5py.File(sys.argv[1], "r")
+#-------------Corner plot function-------------------------------------------------
+def compare(ds1, ds2, ds3, ranges, bins=100, title=''):
+	plt.clf()
+	for i in range(4):
+		plt.subplot(2,2,i+1)
+		plt.hist(ds1[i], bins=bins, range=[ranges[i,0], ranges[i,1]], histtype='step', normed=True, color='red', linewidth=2.)
+		plt.hist(ds2[i], bins=bins, range=[ranges[i,0], ranges[i,1]], histtype='step', normed=True, color='green', linewidth=2.)
+		plt.hist(ds3[i], bins=bins, range=[ranges[i,0], ranges[i,1]], histtype='step', normed=True, color='blue', linewidth=2.)
+		if i==0:
+			e = np.linspace(1.3, 1.3*10, 1000)
+			de = e[1] - e[0]
+			dfde = dfdE(e, 0.3, 1.3)
+			dfde = dfde/np.sum(dfde)/de
+			plt.plot(e, dfde, 'k--', linewidth=1.0)
+		else:
+			p = np.linspace(-1.3*5, 1.3*5, 1000)
+			dp = p[1] - p[0]
+			dfdp = dfdP(p, 0.3, 1.3)
+			dfdp = dfdp/np.sum(dfdp)/dp
+			plt.plot(p, dfdp, 'k--', linewidth=1.0)
+		plt.xlim(ranges[i,0], ranges[i,1])
+	plt.title(title)
+	plt.pause(0.1)
+
+f1 = h5py.File("particledata-22.hdf5", "r")
+f2 = h5py.File("particledata-22-23.hdf5", "r")
+f3 = h5py.File("particledata-22-23-32.hdf5", "r")
 for i in range(100):
-	ds = f['%d'%i].value
-	corner(ds.T, ranges=np.array([[0, 11], [-4, 4], [-4, 4], [-4, 11]]), title="%1.1f"%(i*0.5))
-	
+	ds1 = f1['%d'%i].value
+	ds2 = f2['%d'%i].value
+	ds3 = f3['%d'%i].value
+	#corner(ds.T, ranges=np.array([[0, 11], [-4, 4], [-4, 4], [-4, 11]]), title="%1.1f"%(i*0.5))
+	compare(ds1.T, ds2.T, ds3.T, ranges=np.array([[0, 11], [-4, 4], [-4, 4], [-4, 11]]), title="%1.1f"%(i*0.5))
 plt.show()
--- test.py
+++ test.py
@@ -4,44 +4,8 @@ import sys
 import event
 import h5py
 
-#-------------Thermal distribution function----------------------------------------
-def dfdE(e, T, M):
-	return np.exp(-e/T)*np.sqrt(e**2-M**2)*e
-
-def dfdP(p, T, M):
-	x = np.sqrt(p**2+M**2)/T
-	return (x+1.)*np.exp(-x)
-
-#-------------Corner plot function-------------------------------------------------
-def corner(ds, ranges, bins=50):
-	plt.clf()
-	N = ds.shape[0]
-	for i in range(N):
-		for j in range(i+1):
-			plt.subplot(N, N, i*N+j+1)
-			if i==j:
-				plt.hist(ds[i], bins=bins, range=[ranges[i,0], ranges[i,1]], histtype='step', normed=True)
-				if i==0:
-					e = np.linspace(1.3, 1.3*10, 100)
-					de = e[1] - e[0]
-					dfde = dfdE(e, 0.4, 1.3)
-					dfde = dfde/np.sum(dfde)/de
-					plt.plot(e, dfde, 'r-', linewidth=2.0)
-				else:
-					p = np.linspace(-1.3*5, 1.3*5, 100)
-					dp = p[1] - p[0]
-					dfdp = dfdP(p, 0.4, 1.3)
-					dfdp = dfdp/np.sum(dfdp)/dp
-					plt.plot(p, dfdp, 'r-', linewidth=2.0)
-				plt.xlim(ranges[i,0], ranges[i,1])
-			else:
-				plt.hist2d(ds[j], ds[i], range=[[ranges[j,0], ranges[j,1]],[ranges[i,0], ranges[i,1]]], bins=bins)
-				plt.xlim(ranges[j,0], ranges[j,1])
-				plt.ylim(ranges[i,0], ranges[i,1])
-	plt.pause(0.05)
-
 # A static medium dictionary
-medium = {'Temp': 0.4, 
+medium = {'Temp': 0.3, 
 		  'Vx'	: 0.0, 
 		  'Vy'	: 0.0, 
 		  'Vz'	: 0.0}
@@ -57,7 +21,7 @@ medium = {'Temp': 0.4,
 # 6. table = where to put the tabulated cross-secitons and scattering rates.
 
 # Static Meidum, only elastic
-e1 = event.event(mode='static', elastic=True, inelastic=True, detailed_balance=True, mass=1.3)
+e1 = event.event(mode='static', elastic=True, inelastic=True, detailed_balance=False, mass=1.3)
 
 # Static Meidum, elastic + inelastic
 #e1 = event.event(mode='static', elastic=True, inelastic=True)
@@ -71,7 +35,7 @@ e1 = event.event(mode='static', elastic=True, inelastic=True, detailed_balance=T
 
 e1.initialize_HQ(NQ=10000)
 
-f = h5py.File("particledata.hdf5", 'w')
+f = h5py.File("particledata-22-23.hdf5", 'w')
 plt.figure(figsize=(10, 10))
 for i in range(100):
 	print "t = ", e1.sys_time()
