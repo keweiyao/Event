fix dt units

generate_figs.py
@@ -25,13 +25,13 @@ def corner(ds, ranges, bins=50, title=''):
 					
 					e = np.linspace(1.3, 1.3*10, 100)
 					de = e[1] - e[0]
-					dfde = dfdE(e, 0.3, 1.3)
+					dfde = dfdE(e, 0.4, 1.3)
 					dfde = dfde/np.sum(dfde)/de
 					plt.plot(e, dfde, 'r-', linewidth=2.0)
 				else:
 					p = np.linspace(-1.3*5, 1.3*5, 100)
 					dp = p[1] - p[0]
-					dfdp = dfdP(p, 0.3, 1.3)
+					dfdp = dfdP(p, 0.4, 1.3)
 					dfdp = dfdp/np.sum(dfdp)/dp
 					plt.plot(p, dfdp, 'r-', linewidth=2.0)
 				plt.xlim(ranges[i,0], ranges[i,1])
@@ -53,26 +53,28 @@ def compare(ds1, ds2, ds3, ranges, bins=100, title=''):
 		if i==0:
 			e = np.linspace(1.3, 1.3*10, 1000)
 			de = e[1] - e[0]
-			dfde = dfdE(e, 0.3, 1.3)
+			dfde = dfdE(e, 0.4, 1.3)
 			dfde = dfde/np.sum(dfde)/de
 			plt.plot(e, dfde, 'k--', linewidth=1.0)
 		else:
 			p = np.linspace(-1.3*5, 1.3*5, 1000)
 			dp = p[1] - p[0]
-			dfdp = dfdP(p, 0.3, 1.3)
+			dfdp = dfdP(p, 0.4, 1.3)
 			dfdp = dfdp/np.sum(dfdp)/dp
 			plt.plot(p, dfdp, 'k--', linewidth=1.0)
 		plt.xlim(ranges[i,0], ranges[i,1])
 	plt.title(title)
 	plt.pause(0.1)
 
-f1 = h5py.File("particledata-22.hdf5", "r")
+#f1 = h5py.File("particledata-22.hdf5", "r")
 f2 = h5py.File("particledata-22-23.hdf5", "r")
-f3 = h5py.File("particledata-22-23-32.hdf5", "r")
-for i in range(100):
-	ds1 = f1['%d'%i].value
+#f3 = h5py.File("particledata-22-23-32.hdf5", "r")
+for i in range(300):
+	if i%3 != 0:
+		continue
+	#ds1 = f1['%d'%i].value
 	ds2 = f2['%d'%i].value
-	ds3 = f3['%d'%i].value
-	#corner(ds.T, ranges=np.array([[0, 11], [-4, 4], [-4, 4], [-4, 11]]), title="%1.1f"%(i*0.5))
-	compare(ds1.T, ds2.T, ds3.T, ranges=np.array([[0, 11], [-4, 4], [-4, 4], [-4, 11]]), title="%1.1f"%(i*0.5))
+	#ds3 = f3['%d'%i].value
+	corner(ds2.T, ranges=np.array([[0, 11], [-4, 4], [-4, 4], [-4, 11]]), title="%1.1f"%(i*0.3))
+	#compare(ds1.T, ds2.T, ds3.T, ranges=np.array([[0, 11], [-4, 4], [-4, 4], [-4, 11]]), title="%1.1f"%(i*0.5))
 plt.show()

plotX.py
@@ -34,33 +34,30 @@ plt.show()
 for it, dt in enumerate(dtx):
 	plt.subplot(2, 5, it+1)
 	for iT, T in enumerate(t34):
-		scale = 1.0*dt**2/T**2
+		scale = dt/T**2
 		plt.plot(s34, x3[it, iT]/scale, 'b-')
 		plt.plot(s34, x4[it, iT]/scale, 'y-')
 	#plt.ylim(1e-3, 1e0)
 	#plt.semilogy()
 plt.show()
-"""
 
 
-"""
+
 N = 10
 A1 = np.linspace(0.501, 0.999, 20)
-A2 = np.linspace(-0.999, 0.999, 10)
+A2 = np.linspace(-0.999, 0.999, 20)
 for i1, a1 in enumerate(A1[::2]):
-	for i2, a2 in enumerate(A2[::1]):
+	for i2, a2 in enumerate(A2[::2]):
 		plt.subplot(N, N, i1*N + i2 + 1)
 		for iT, T in enumerate(t56):
-			xk = 0.5*(a1*a2 + a1 - a2)
 			x2 = 0.5*(-a1*a2 + a1 + a2)
-			scale = s56**2/T/T/x2
-			plt.plot(s56, f5[i2, 2*i1, iT]/scale, 'k-')
-			plt.plot(s56, f6[i2, 2*i1, iT]/scale, 'c-')
+			scale = (s56**2 - M2)/T**2/x2
+			plt.plot(s56, f5[2*i2, 2*i1, iT]/scale, 'k-')
+			plt.plot(s56, f6[2*i2, 2*i1, iT]/scale, 'c-')
 		#plt.ylim(0, 200)
 		#plt.semilogy()
 plt.show()
 """
-
 
 def myplot(folder):
 	Er = np.linspace(1.01*M, 100.*M, 100)