be7015ec029d74b51a5f7f81edfc0b7c39ecde36
Weiyao Ke <wk42@grads-f3.internal.phy.duke.edu>
2017-01-21 17:48:39 -0500

    use pretabulated scattering table

--- plotX.py
+++ plotX.py
@@ -1,103 +1,168 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import h5py
 
 #folder = sys.argv[1]
 M = 1.3
 M2 = M**2
 GeVm2_to_mb = 0.3849
 """
-s12 = np.concatenate([np.linspace(1.01*M, 5.*M, 50), np.linspace(5.*M2, 30.*M, 50)])
-s34 = np.linspace(1.01*M, 30.*M, 50)
+f1 = h5py.File('./tables/XQq2Qq.hdf5')
+x1 = f1['Xsection-tab'].value
+at = f1['Xsection-tab'].attrs
+ss1 = np.concatenate([	np.linspace(at['sqrts_low'], at['sqrts_mid'], at['N_sqrt_half']),
+						np.linspace(at['sqrts_mid'], at['sqrts_high'], at['N_sqrt_half'])])
+T1 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+f1.close()
 
-s56 = np.linspace(1.01*M, 30.*M, 60)
-t12 = np.linspace(0.1, 0.8, 32)
-t34 = np.linspace(0.1, 0.8, 16)
-t56 = np.linspace(0.1, 0.8, 8)
-dtx = np.linspace(0.1, 5., 10.)
+f2 = h5py.File('./tables/XQg2Qg.hdf5')
+x2 = f2['Xsection-tab'].value
+at = f2['Xsection-tab'].attrs
+ss2 = np.concatenate([	np.linspace(at['sqrts_low'], at['sqrts_mid'], at['N_sqrt_half']),
+						np.linspace(at['sqrts_mid'], at['sqrts_high'], at['N_sqrt_half'])])
+T2 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+f2.close()
 
-x1 = np.loadtxt('./tables/XQq2Qq.dat').reshape(100, 32).T*GeVm2_to_mb
-x2 = np.loadtxt('./tables/XQg2Qg.dat').reshape(100, 32).T*GeVm2_to_mb
-x3 = np.loadtxt('./tables/XQq2Qqg.dat').reshape(50, 16, 10).T*GeVm2_to_mb
-x4 = np.loadtxt('./tables/XQg2Qgg.dat').reshape(50, 16, 10).T*GeVm2_to_mb
-f5 = np.loadtxt('./tables/XQqg2Qq.dat').reshape(60, 8, 20, 20).T
-f6 = np.loadtxt('./tables/XQgg2Qg.dat').reshape(60, 8, 20, 20).T
+f3 = h5py.File('./tables/XQq2Qqg.hdf5')
+x3 = f3['Xsection-tab'].value
+at = f3['Xsection-tab'].attrs
+ss3 = np.linspace(at['sqrts_low'], at['sqrts_high'], at['N_sqrt_half'])
+T3 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+dt3 = np.linspace(at['dt_low'], at['dt_high'], at['N_dt'])
+f3.close()
 
+f4 = h5py.File('./tables/XQg2Qgg.hdf5')
+x4 = f4['Xsection-tab'].value
+at = f4['Xsection-tab'].attrs
+ss4 = np.linspace(at['sqrts_low'], at['sqrts_high'], at['N_sqrt_half'])
+T4 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+dt4 = np.linspace(at['dt_low'], at['dt_high'], at['N_dt'])
+f4.close()
 
+f5 = h5py.File('./tables/XQqg2Qq.hdf5')
+x5 = f5['Xsection-tab'].value
+at = f5['Xsection-tab'].attrs
+ss5 = np.linspace(at['sqrts_low'], at['sqrts_high'], at['N_sqrt_half'])
+T5 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+a15 = np.linspace(at['a1_low'], at['a1_high'], at['N_a1'])
+a25 = np.linspace(at['a2_low'], at['a2_high'], at['N_a2'])
+f5.close()
 
-for i, T in enumerate(t12):
-	scale1 = 1.0/T**2
-	scale2 = 1.0/(1.0 - M2/s12**2)**2/T**2
-	plt.plot(s12, x1[i]/scale1, 'r-')
-	plt.plot(s12, x2[i]/scale2, 'g-')
+f6 = h5py.File('./tables/XQgg2Qg.hdf5')
+x6 = f6['Xsection-tab'].value
+at = f6['Xsection-tab'].attrs
+ss6 = np.linspace(at['sqrts_low'], at['sqrts_high'], at['N_sqrt_half'])
+T6 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+a65 = np.linspace(at['a1_low'], at['a1_high'], at['N_a1'])
+a65 = np.linspace(at['a2_low'], at['a2_high'], at['N_a2'])
+f6.close()
+
+for i, T in enumerate(T1):
+	scale1 = 1.0#/T**2
+	scale2 = 1.0#/(1.0 - M2/ss1**2)**2/T**2
+	plt.plot(ss1, x1[:,i]*scale1, 'r-')
+	plt.plot(ss2, x2[:,i]*scale2, 'g-')
 plt.show()
 
-for it, dt in enumerate(dtx):
+for it, dt in enumerate(dt3):
 	plt.subplot(2, 5, it+1)
-	for iT, T in enumerate(t34):
-		scale = dt/T**2
-		plt.plot(s34, x3[it, iT]/scale, 'b-')
-		plt.plot(s34, x4[it, iT]/scale, 'y-')
-	#plt.ylim(1e-3, 1e0)
-	#plt.semilogy()
+	for iT, T in enumerate(T3):
+		scale = 1.0#*dt**2/T**2
+		plt.plot(ss3, x3[:, iT, it]*scale, 'b-')
+		plt.plot(ss4, x4[:, iT, it]*scale, 'y-')
 plt.show()
-
 
 
 N = 10
-A1 = np.linspace(0.501, 0.999, 20)
-A2 = np.linspace(-0.999, 0.999, 20)
-for i1, a1 in enumerate(A1[::2]):
-	for i2, a2 in enumerate(A2[::2]):
+for i1, a1 in enumerate(a15[::2]):
+	for i2, a2 in enumerate(a25[::2]):
 		plt.subplot(N, N, i1*N + i2 + 1)
-		for iT, T in enumerate(t56):
+		for iT, T in enumerate(T5):
 			x2 = 0.5*(-a1*a2 + a1 + a2)
-			scale = (s56**2 - M2)/T**2/x2
-			plt.plot(s56, f5[2*i2, 2*i1, iT]/scale, 'k-')
-			plt.plot(s56, f6[2*i2, 2*i1, iT]/scale, 'c-')
-		#plt.ylim(0, 200)
-		#plt.semilogy()
+			scale = 1.#*(ss5**2 - M2)/T**2/x2
+			plt.plot(ss5, x5[:, iT, 2*i1, 2*i2]*scale, 'k-')
+			plt.plot(ss6, x6[:, iT, 2*i1, 2*i2]*scale, 'c-')
 plt.show()
 """
 
-def myplot(folder):
-	Er = np.linspace(1.01*M, 100.*M, 100)
-	Er2 = np.linspace(1.01*M, 30.*M, 30)
-	tr = np.linspace(0.13, 0.75, 8)
-	dtr = np.linspace(0.1, 5., 10.)
-	dtr2 = np.linspace(0.1, 5., 10.)
+f1 = h5py.File('./tables/RQq2Qq.hdf5')
+R1 = f1['Rates-tab'].value
+at = f1['Rates-tab'].attrs
+E1 = np.concatenate([	np.linspace(at['E1_low'], at['E1_high'], at['N_E1'])])
+T1 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+f1.close()
 
-	r1 = np.loadtxt('./%s/RQq2Qq.dat'%folder).reshape(100, 16).T
-	r2 = np.loadtxt('./%s/RQg2Qg.dat'%folder).reshape(100, 16).T
-	r3 = np.loadtxt('./%s/RQq2Qqg.dat'%folder).reshape(100, 8, 10).T
-	r4 = np.loadtxt('./%s/RQg2Qgg.dat'%folder).reshape(100, 8, 10).T
-	r5 = np.loadtxt('./%s/RQqg2Qq.dat'%folder).reshape(30, 8, 10).T
-	r6 = np.loadtxt('./%s/RQgg2Qg.dat'%folder).reshape(30, 8, 10).T
+f2 = h5py.File('./tables/RQg2Qg.hdf5')
+R2 = f2['Rates-tab'].value
+at = f2['Rates-tab'].attrs
+E2 = np.concatenate([	np.linspace(at['E1_low'], at['E1_high'], at['N_E1'])])
+T2 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+f2.close()
 
-	
-	for it, dt in enumerate(dtr):
-		plt.subplot(2, 5, it+1)
-		for iT, T in enumerate(tr):
-			scale12 = 1.#T**0.4
-			plt.plot(Er, r1[iT]/scale12, 'r-', lw=1, label=r'$Qq\rightarrow Qq$' if iT==0 else '')
-			plt.plot(Er, r2[iT]/scale12, 'g-', lw=1, label=r'$Qg\rightarrow Qg$' if iT==0 else '')
-			scale34 = 1.#dt**2/Er**0.75*T**2.6
-			plt.plot(Er, r3[it, iT]/scale34, 'y-', lw=2, label=r'$Qq\rightarrow Qqg$' if iT==0 else '')
-			plt.plot(Er, r4[it, iT]/scale34, 'b-', lw=2, label=r'$Qg\rightarrow Qgg$' if iT==0 else '')
-			scale56 = 1.#dtr2[it]**2/Er2**1.4*T**3.5
-			plt.plot(Er2, r5[it, iT]/scale56, 'y--', lw=2, label=r'$Qqg\rightarrow Qq$' if iT==0 else '')
-			plt.plot(Er2, r6[it, iT]/scale56, 'b--', lw=2, label=r'$Qgg\rightarrow Qg$' if iT==0 else '')
-		#plt.ylim(0, 1.5)
-		#plt.semilogy()
+f3 = h5py.File('./tables/RQq2Qqg.hdf5')
+R3 = f3['Rates-tab'].value
+at = f3['Rates-tab'].attrs
+E3 = np.concatenate([	np.linspace(at['E1_low'], at['E1_high'], at['N_E1'])])
+T3 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+dt3 = np.linspace(at['dt_low'], at['dt_high'], at['N_dt'])
+f3.close()
 
+f4 = h5py.File('./tables/RQg2Qgg.hdf5')
+R4 = f4['Rates-tab'].value
+at = f4['Rates-tab'].attrs
+E4 = np.concatenate([	np.linspace(at['E1_low'], at['E1_high'], at['N_E1'])])
+T4 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+dt4 = np.linspace(at['dt_low'], at['dt_high'], at['N_dt'])
+f4.close()
 
-plt.figure(figsize=(15, 6))
+f5 = h5py.File('./tables/RQqg2Qq.hdf5')
+R5 = f5['Rates-tab'].value
+at = f5['Rates-tab'].attrs
+E5 = np.concatenate([	np.linspace(at['E1_low'], at['E1_high'], at['N_E1'])])
+T5 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+dt5 = np.linspace(at['dt_low'], at['dt_high'], at['N_dt'])
+f5.close()
 
-myplot("tables")
+f6 = h5py.File('./tables/RQgg2Qg.hdf5')
+R6 = f6['Rates-tab'].value
+at = f6['Rates-tab'].attrs
+E6 = np.concatenate([	np.linspace(at['E1_low'], at['E1_high'], at['N_E1'])])
+T6 = np.linspace(at['T_low'], at['T_high'], at['N_T'])
+dt6 = np.linspace(at['dt_low'], at['dt_high'], at['N_dt'])
+f6.close()
 
+plt.figure(figsize=(15, 8))
 
-plt.legend(loc='best', fontsize=20, framealpha=0.0)
-plt.ylabel(r"$\Gamma$ [GeV]", size=20)
-plt.xlabel(r"$E_{HQ}$ [GeV]", size=20)
+for it, dt in enumerate(dt3):
+	plt.subplot(2, 5, it+1)
+	for iT, T in enumerate(T1):
+		scale12 = 1.#T**0.4
+		plt.plot(E1, R1[:, iT]/scale12, 'r-', lw=1, label=r'$Qq\rightarrow Qq$' if iT==0 and it==0 else '')
+		plt.plot(E2, R2[:, iT]/scale12, 'g-', lw=1, label=r'$Qg\rightarrow Qg$' if iT==0 and it==0 else '')
+	for iT, T in enumerate(T3):
+		scale34 = 1.#dt**2/Er**0.75*T**2.6
+		plt.plot(E3, R3[:, iT, it]/scale34, 'y-', lw=2, label=r'$Qq\rightarrow Qqg$' if iT==0 and it==1 else '')
+		plt.plot(E4, R4[:, iT, it]/scale34, 'b-', lw=2, label=r'$Qg\rightarrow Qgg$' if iT==0 and it==1 else '')
+		scale56 = 1.#dtr2[it]**2/Er2**1.4*T**3.5
+		plt.plot(E5, R5[:, iT, it]/scale56, 'y--', lw=2, label=r'$Qqg\rightarrow Qq$' if iT==0 and it==2 else '')
+		plt.plot(E6, R6[:, iT, it]/scale56, 'b--', lw=2, label=r'$Qgg\rightarrow Qg$' if iT==0 and it==2 else '')
+	plt.ylim(0, 1.1)
+	if it == 0 or it == 5:
+		plt.ylabel(r"$\Gamma$ [GeV]", size=20)
+	if it >= 5:
+		plt.xlabel(r"$E_{HQ}$ [GeV]", size=20)
+	if it <5 and it > 0:
+		plt.xticks([])
+		plt.yticks([])
+	if it <10 and it > 5:
+		plt.yticks([])
+	if it==0:
+		plt.xticks([])
+	plt.legend(loc='best', fontsize=20, framealpha=0.0)
+	plt.title(r"$\langle \Delta t\rangle = %1.1f$ fm/c"%dt, size=15)
+
+plt.subplots_adjust(wspace=0., hspace=0.15)
 plt.show()
+
 
