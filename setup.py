33f3e5000e9c5d56d99b681b91ee34b24ebb98b3
YingruXu <yx59@phy.duke.edu>
2017-03-16 13:54:21 -0400

    Fix the bug of stuck... read in transport as class member

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ extensions = [
 		filelist1,
 		language="c++",
 		extra_compile_args=["-std=c++11"],
-		include_path = [numpy.get_include()],
 		libraries=["m"])
 ]
 
 setup(
-    ext_modules=cythonize(extensions)
+    ext_modules=cythonize(extensions),
+    include_dirs=[numpy.get_include()]
         )
 
