f6e2efcbc3b219bc0971041890530a29643a17e4
YingruXu <yx59@phy.duke.edu>
2017-03-17 16:32:32 -0400

    nonthing

--- test.py
+++ test.py
@@ -19,29 +19,29 @@ from scipy.interpolate import interp1d
 # A static medium dictionary
 #medium = {'Temp': 0.3, 'Vx'	: 0.0, 'Vy'	: 0.0, 'Vz'	: 0.0}
 #e1 = event.event(mode='static', static_dt=1., elastic=True, inelastic=True, detailed_balance=True, mass=1.3)
-#e1 = event.event(mode='static', static_dt=1., elastic=True, inelastic=False, detailed_balance=False, mass=1.3)
+#e1 = event.event(mode='static', transport='LGV', static_dt=1., elastic=True, inelastic=False, detailed_balance=False, EinR=True, mass=1.3)
 
 # Dynamic Meidum
-e1 = event.event(mode='dynamic', hydrofile=sys.argv[2], inelastic=True, detailed_balance=True, mass=1.3)
-#e1 = event.event(mode='dynamic', hydrofile=sys.argv[2], inelastic=False, detailed_balance=False, mass=1.3)
-f = h5py.File("particledata.hdf5", 'w')
+#e1 = event.event(mode='dynamic', hydrofile=sys.argv[2], inelastic=True, detailed_balance=True, mass=1.3)
+e1 = event.event(mode='dynamic', transport='LGV', hydrofile=sys.argv[2], elastic=True, inelastic=False, detailed_balance=False, EinR=False, mass=1.3)
+f = h5py.File("particledata_test_LGV.hdf5", 'w')
 
 # Initialize HQ pT weight and xy-distribution
 
-TR = np.loadtxt(sys.argv[1]).T ## p=0.
+TR = np.loadtxt(sys.argv[1]).T ## p=0.   ##? why you need to rotate TR?
 Taa = TR**2
 
-pt, dsigma = np.loadtxt(sys.argv[3]).T
+pt, dsigma = np.loadtxt(sys.argv[3], unpack=True)
 dfdpt2 = interp1d(pt, dsigma)
 def pT_weight(pt, y):
 	return dfdpt2(pt)
 
-e1.initialize_HQ(NQ=100000, Taa=Taa, dxy=0.1, df_dpt2dy=pT_weight, oversample_power=-1.)
+e1.initialize_HQ(NQ=1000000, Taa=Taa, dxy=0.1, df_dpt2dy=pT_weight, oversample_power=-1.)
 
 # Run Modgetel  
-for i in range(400):
+for i in range(1000):
 	print "t = %1.2f [fm/c]"%e1.sys_time()
-	status = e1.perform_hydro_step()#(StaticPropertyDictionary=medium)
+	status = e1.perform_hydro_step() #StaticPropertyDictionary=medium)
 	if i%10 == 0:
 		dsp, dsx, dsw = e1.HQ_hist()
 		f.create_dataset('p-%d'%i, data=dsp)
